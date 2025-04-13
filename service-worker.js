d1820f0f16fc96f2fa1e65dc802bc68ad7eb9ac6
yanncoad <yann.coadour@ac-rennes.fr>
2025-04-13 13:05:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,32 @@
-const CACHE_NAME = 'dailyflow-cache-v1';
+const CACHE_NAME = 'yannapp-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
+    '/sport.html',
+    '/lbb.html',
     '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
+    '/logo.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames.map(name => {
+                if (name !== CACHE_NAME) return caches.delete(name);
+            })
+        ))
     );
 });
